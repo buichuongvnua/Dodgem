31dcb448297ae623c2fa5edea890afa704dd1a53
Bùi Văn Chương <Bi Vn Chng>
2016-11-06 21:31:46 +0700

    Start code Dodgem!

---
+++ Dodgem/Class/Cell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodgem.Class
+{
+    class Cell:PictureBox
+    {
+        private int X;
+        private int Y;
+        private char Status;
+        private bool Selected;
+
+        public Cell(char type, int x, int y)
+        {
+            Status = type;
+            X = x;
+            Y = y;     
+        }
+        public StatusBar(char type)
+        {
+
+        }
+
+
+    }
+}
